896a4c4cde0b3e2130dfa38df4a78933e75946f6
miolfo <mikko.o.forsman@gmail.com>
2018-05-17 19:12:38 +0300

    Sending messages to firebase

--- src/clojure_chat/handler.clj
+++ src/clojure_chat/handler.clj
@@ -16,8 +16,8 @@
     (clojure.string/join "<br>" (map #(str (get-in messages [% :sender]) ": " (get-in messages [% :message])) (keys messages)))))
 
 (defn send-message [request]
-  (str (:multipart-params request)))
-
+  (client/post (slurp "messages-url.txt") {:form-params (:multipart-params request) :content-type :json}))
+          
 (defroutes api-routes
   (GET "/messages" []
     (rr/response (parse-message-string)))
@@ -35,4 +35,3 @@
           (-> api-routes
               (ring-json/wrap-json-response)
               (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false)))))
-
